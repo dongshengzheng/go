a9a61ae29514e26dafe868843101f48c7d5b24ac
dongsheng.zheng@windyeel.com <ease4East>
2017-01-16 10:44:50 +0800

    deleted unrelated entities and sys_dict

--- admin/src/main/java/com/ctoangels/go/admin/modules/go/controller/WorksController.java
+++ admin/src/main/java/com/ctoangels/go/admin/modules/go/controller/WorksController.java
@@ -4,43 +4,22 @@ import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.ctoangels.go.admin.controller.BaseController;
-import com.ctoangels.go.common.modules.go.entity.Consumer;
-import com.ctoangels.go.common.modules.go.entity.Report;
 import com.ctoangels.go.common.modules.go.entity.Works;
-import com.ctoangels.go.common.modules.go.entity.WorksLevel;
-import com.ctoangels.go.common.modules.go.service.IConsumerService;
-import com.ctoangels.go.common.modules.go.service.IReportService;
-import com.ctoangels.go.common.modules.go.service.IWorksLevelService;
 import com.ctoangels.go.common.modules.go.service.IWorksService;
 import com.ctoangels.go.common.util.Const;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 /**
- *
  * Works 控制层
- *
  */
 @Controller
 @RequestMapping("works")
-public class WorksController  extends BaseController {
-
-    @Autowired
-    private IWorksLevelService worksLevelService;
-
-    @Autowired
-    private IReportService reportService;
-
-    @Autowired
-    private IConsumerService consumerService;
-
+public class WorksController extends BaseController {
     @Autowired
     private IWorksService worksService;
-
-
 
     @RequestMapping
     public String page() {
@@ -51,67 +30,36 @@ public class WorksController  extends BaseController {
     @ResponseBody
     public JSONObject list(Works works) {
         EntityWrapper<Works> ew = getEntityWrapper();
-        ew.addFilter("status <> {0}",Const.WORKS_STATUS_DRAFT);
+        ew.addFilter("status <> {0}", Const.WORKS_STATUS_DRAFT);
         if (!StringUtils.isEmpty(works.getName()))
-            ew.addFilter("name={0}",works.getName());
-       Page<Works> page = worksService.selectPage(getPage(), ew);
-       for (Works w:page.getRecords()){
-           Consumer collect = consumerService.selectOne(new EntityWrapper<>(new Consumer(Const.CONSUMER_TYPE_COLLECT,works.getId())));
-           w.setCollector(collect.getName());
-       }
+            ew.addFilter("name={0}", works.getName());
+        Page<Works> page = worksService.selectPage(getPage(), ew);
+        for (Works works_infor : page.getRecords()){
+            works_infor.setCollector("James");
+        }
         return jsonPage(page);
     }
 
     /**
      * 审核作品
      */
-    @RequestMapping(value = "check",method = RequestMethod.POST)
+    @RequestMapping(value = "check", method = RequestMethod.POST)
     @ResponseBody
     public Boolean check(Works works) {
-        return worksService.updateSelectiveById(new Works(works.getId(),works.getStatus()));
+        return worksService.updateSelectiveById(new Works(works.getId(), works.getStatus()));
 
     }
 
     /**
      * 作品轮播
      */
-    @RequestMapping(value = "slide",method = RequestMethod.POST)
+    @RequestMapping(value = "slide", method = RequestMethod.POST)
     @ResponseBody
-    public Boolean slide(Integer id,Integer slide) {
+    public Boolean slide(Integer id, Integer slide) {
         Works works = new Works();
         works.setId(id);
         works.setSlide(slide);
         return worksService.updateSelectiveById(works);
 
-    }
-
-    @RequestMapping(value = "/level", method = RequestMethod.GET)
-    public String level(@RequestParam Integer worksId, ModelMap map) {
-        WorksLevel worksLevel = worksLevelService.selectOne(new WorksLevel(worksId));
-        map.put("worksLevel", worksLevel);
-        return "go/works/level";
-    }
-
-
-    @RequestMapping(value = "/provider", method = RequestMethod.GET)
-    public String provider(@RequestParam Integer worksId, ModelMap map) {
-        Consumer provider = consumerService.selectOne(new Consumer(Const.CONSUMER_TYPE_PROVIDER,worksId));
-        map.put("provider", provider);
-        return "go/works/provider";
-    }
-
-    @RequestMapping(value = "/collect", method = RequestMethod.GET)
-    public String collect(@RequestParam Integer worksId, ModelMap map) {
-        Consumer collect = consumerService.selectOne(new Consumer(Const.CONSUMER_TYPE_COLLECT,worksId));
-        map.put("collect", collect);
-        return "go/works/collect";
-    }
-
-
-    @RequestMapping(value = "/report", method = RequestMethod.GET)
-    public String toEdit(@RequestParam Integer worksId, ModelMap map) {
-        Report works = reportService.selectOne(new Report(worksId));
-        map.put("works", works);
-        return "go/works/report";
     }
 }
